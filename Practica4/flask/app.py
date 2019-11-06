b4f4618a5a5fc0c11534dbaa11b6fd683967a0ad
José Manuel Pérez Lendínez <jmplz14@correo.ugr.es>
2019-11-07 00:30:18 +0100

    Terminada practica 4

---
+++ Practica4/flask/app.py
@@ -0,0 +1,158 @@
+from flask import Flask,  render_template, url_for, send_file, request, session
+from PIL import Image
+from random import randrange
+from pickleshare import *
+from pymongo import MongoClient
+
+client = MongoClient("mongo", 27017) # Conectar al servicio (docker) "mongo" en su puerto estandar
+db1 = client.SampleCollections 
+
+
+app = Flask(__name__)
+app.secret_key = 'esto-es-una-clave-muy-secreta'
+
+historial = []
+db = PickleShareDB('usuarios/datosUsuarios')
+
+@app.route('/mongo')
+def mongo():
+    nombre = str(session['usuario'])
+    nombre = nombre.upper()
+    val = db1.Sakila_actors.find({ "FirstName": nombre })  # Encontramos los documentos de la coleccion "samples_friends"
+    nombreActores = []
+    anadirHistorial("Actores")
+    for i in val:
+        actor = i["FirstName"] + " " + i["LastName"] 
+        nombreActores.append(actor)
+    if (len(nombreActores) > 0):
+        return render_template("mongo.html", usuario = session['usuario'], arrayHistorial=historial, actores = nombreActores )
+    else:
+        return render_template("mongo.html",  usuario = session['usuario'], arrayHistorial=historial)
+
+def anadirHistorial(pagina):
+    if(len(historial) < 3):
+        historial.append(pagina)
+    else:
+        historial.pop(0)
+        historial.append(pagina)
+
+def hayUser():
+    return  'usuario' in session
+def existeUsuario(usuario):
+    return  db.keys(usuario)
+    
+@app.route('/login',methods=['POST'])
+def login():
+    nombre = request.form['usuario']
+    contraseña = request.form['contraseña']
+    if existeUsuario(nombre):
+        if contraseña == db[nombre]['contraseña']:
+            session['usuario'] = nombre
+            #session['contraseña'] = contraseña
+    return index()
+
+
+@app.route('/logout',methods=['POST'])
+def logout():
+    session.clear()
+    del historial[:]
+    return render_template("index.html")
+
+
+
+
+@app.route("/productos")
+def inicio():
+    if hayUser():
+        anadirHistorial("productos")
+        return render_template("productos.html",usuario = session['usuario'],arrayHistorial=historial)
+        
+    else:
+        return render_template("productos.html")
+
+@app.route("/registro",methods=['POST'])
+def registro():   
+    return render_template("registro.html")
+    
+@app.route("/registrado",methods=['POST'])
+def registrar(): 
+    nombre = request.form['nombre']
+    contraseña = request.form['contraseña']
+    nombreCompleto = request.form['nombreCompleto']
+    if nombre != "" and contraseña != "" and nombreCompleto != "":
+        if not existeUsuario(nombre):
+            session['usuario'] = nombre
+            #session['contraseña'] = contraseña
+            db[nombre] = {'contraseña': contraseña, 'nombreCompleto': nombreCompleto} 
+            return index()
+    return render_template("registro.html", cambiado = False)
+
+@app.route("/cambioDatos")
+def cambioDatos():   
+    if hayUser():
+        anadirHistorial("Datos usuario")
+        nombre = db[session["usuario"]]['nombreCompleto']
+        return render_template("cambioDatos.html",usuario = session['usuario'],arrayHistorial=historial,session = session, nombreCompleto = nombre)
+    else:
+        return render_template("cambioDatos.html")
+
+
+
+@app.route("/cambiados",methods=['POST'])
+def cambiados():
+    nombre = request.form['nombre']
+    contraseña = request.form['contraseña']
+    nombreCompleto = request.form['nombreCompleto']
+    cambiado = False
+    if nombre != session['usuario']:
+        if not existeUsuario(nombre):
+            db[nombre] = {'contraseña': db[session['usuario']]['contraseña'], 'nombreCompleto': db[session['usuario']]['nombreCompleto']}
+            db.pop(session['usuario'])
+            session['usuario'] = nombre
+            cambiado = True
+        if cambiado:
+            if contraseña != "":
+                db[nombre] = {'contraseña': contraseña, 'nombreCompleto': nombreCompleto} 
+        
+            if nombreCompleto != db[nombre]['nombreCompleto']:
+                db[nombre] = {'contraseña': db[nombre]['contraseña'], 'nombreCompleto': nombreCompleto} 
+    else:
+        if contraseña != "":
+            db[nombre] = {'contraseña': contraseña, 'nombreCompleto': nombreCompleto}
+            cambiado = True
+        
+        if nombreCompleto != db[nombre]['nombreCompleto']:
+            db[nombre] = {'contraseña': db[nombre]['contraseña'], 'nombreCompleto': nombreCompleto} 
+            cambiado = True
+
+            
+    
+    if cambiado: 
+        return render_template("cambioDatos.html",usuario = session['usuario'],arrayHistorial=historial,session = session, nombreCompleto = nombreCompleto, cambiado = True)
+    else:
+        return render_template("cambioDatos.html",usuario = session['usuario'],arrayHistorial=historial,session = session, nombreCompleto = nombreCompleto, cambiado = False)
+    
+
+
+@app.route("/inicio")
+def productos():
+    if hayUser():
+        anadirHistorial("inicio")
+        return render_template("inicio.html",usuario = session['usuario'],arrayHistorial=historial)
+    else:
+        return render_template("inicio.html")
+
+@app.route("/")
+def index():
+
+    if hayUser():
+	    return render_template("index.html",usuario = session['usuario'],arrayHistorial=historial)
+    else:
+        return render_template("index.html")
+
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+# defining function 
+  return '''Error 404'''
+
